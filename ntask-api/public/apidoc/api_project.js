e13863e2898ff34cf5b5146c8f5eaa92b99fb871
Paulo Mariano <paulotiagocm@hotmail.com>
2020-10-18 19:13:46 -0300

    apidoc init

---
+++ ntask-api/public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Docs API TASK",
+  "template": {
+    "forceLanguage": "pt_br"
+  },
+  "version": "1.0.0",
+  "description": "API task manager to study",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-18T22:10:50.232Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
--- ntask-api/routes/all.js
+++ ntask-api/routes/all.js
@@ -1,5 +1,20 @@
 module.exports = (app) => {
   const allRoutes = app.routes;
+  /**
+   * @api {get} / API status
+   * @apiGroup Status
+   * @apiSuccess {String} status OK da API + todas rotas
+   * @apiSuccessExample {json} sucesso
+   *     HTTP/1.1 200 OK
+   *     {
+            "request": {
+              "path": "/",
+              "methods": {
+                "get": true
+              }
+            }
+          }
+   */
   app.get("/", (req, res) => {
     res.json({
       request: req.route,
